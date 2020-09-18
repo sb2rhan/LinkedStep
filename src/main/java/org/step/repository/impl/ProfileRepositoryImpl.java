d2054e52d39d137a4d6c3cd73eef0356130173c9
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-09-18 21:58:44 +0600

    added some tests to ProfileRepositoryTest

--- src/main/java/org/step/repository/impl/ProfileRepositoryImpl.java
+++ src/main/java/org/step/repository/impl/ProfileRepositoryImpl.java
@@ -1,29 +1,70 @@
 package org.step.repository.impl;
 
+import org.hibernate.annotations.QueryHints;
 import org.step.entity.Profile;
 import org.step.repository.ProfileRepository;
+import org.step.repository.SessionFactoryCreator;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 public class ProfileRepositoryImpl implements ProfileRepository {
+    private static final EntityManager ENTITY_MANAGER = SessionFactoryCreator.getEntityManager();
 
     @Override
     public List<Profile> findAll() {
-        return null;
+        ENTITY_MANAGER.getTransaction().begin();
+
+        List<Profile> profiles = ENTITY_MANAGER
+                .createQuery("select p from Profile p", Profile.class)
+                .setHint(QueryHints.READ_ONLY, true)
+                .getResultList();
+
+        ENTITY_MANAGER.getTransaction().commit();
+
+        return profiles;
     }
 
     @Override
     public Profile findById(Long id) {
-        return null;
+        ENTITY_MANAGER.getTransaction().begin();
+
+        Profile profile = ENTITY_MANAGER
+                .createQuery("select p from Profile p where p.id=:id", Profile.class)
+                .setHint(QueryHints.READ_ONLY, true)
+                .setParameter("id", id)
+                .getSingleResult();
+
+        ENTITY_MANAGER.getTransaction().commit();
+
+        return profile;
     }
 
     @Override
     public List<Profile> findByFullName(String fullName) {
-        return null;
+        ENTITY_MANAGER.getTransaction().begin();
+
+        List<Profile> profiles = ENTITY_MANAGER
+                .createQuery("select p from Profile p where p.fullName=:fullName", Profile.class)
+                //.setHint(QueryHints.READ_ONLY, true)
+                .setParameter("fullName", fullName)
+                .getResultList();
+
+        ENTITY_MANAGER.getTransaction().commit();
+
+        return profiles;
     }
 
     @Override
     public void deleteProfile(Long id) {
+        ENTITY_MANAGER.getTransaction().begin();
 
+        ENTITY_MANAGER.createQuery("delete from Profile p where p.id=:id")
+                .setParameter("id", id)
+                .executeUpdate();
+
+        System.out.printf("Profile with id %d has been deleted\n", id);
+
+        ENTITY_MANAGER.getTransaction().commit();
     }
 }
--- src/test/java/org/step/repository/ProfileRepositoryTest.java
+++ src/test/java/org/step/repository/ProfileRepositoryTest.java
@@ -8,9 +8,9 @@ import org.step.entity.Profile;
 import org.step.repository.impl.ProfileRepositoryImpl;
 
 import javax.persistence.EntityManager;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class ProfileRepositoryTest {
 
@@ -21,9 +21,10 @@ public class ProfileRepositoryTest {
     @Before
     public void setup() {
         Profile profile = Profile.builder()
-                .abilities("abilities")
-                .graduation("graduation")
-                .workExperience("experience")
+                .fullName("Markus Bat")
+                .abilities("writes only clean code")
+                .graduation("Software engineer")
+                .workExperience("2 years")
                 .build();
 
         entityManager.getTransaction().begin();
@@ -42,5 +43,50 @@ public class ProfileRepositoryTest {
         entityManager.createQuery("delete from Profile p").executeUpdate();
 
         entityManager.getTransaction().commit();
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Profile> allProfiles = profileRepository.findAll();
+
+        Assert.assertNotNull(allProfiles);
+        Assert.assertFalse(allProfiles.isEmpty());
+        Assert.assertTrue(allProfiles.contains(Profile.builder().id(profiles.get(0).getId()).build()));
+    }
+
+    @Test
+    public void testFindById() {
+        final int chosenIndex = 0;
+        final Long testId = profiles.get(chosenIndex).getId();
+        Profile profile = profileRepository.findById(testId);
+
+        Assert.assertNotNull(profile);
+        Assert.assertEquals(profile, profiles.get(chosenIndex));
+    }
+
+    @Test
+    public void testFindByFullName() {
+        final int chosenIndex = 0;
+        final String testFullName = profiles.get(chosenIndex).getFullName();
+        List<Profile> profileList = profileRepository.findByFullName(testFullName);
+        profileList.forEach(System.out::println);
+
+        Assert.assertNotNull(profileList);
+        Assert.assertFalse(profileList.isEmpty());
+        Assert.assertTrue(profileList.contains(profiles.get(chosenIndex)));
+    }
+
+    @Test
+    public void testDeleteProfile() {
+        final int chosenIndex = 0;
+        final Long testId = profiles.get(chosenIndex).getId();
+        profileRepository.deleteProfile(testId);
+
+        BigInteger profileSingleResult = (BigInteger) entityManager
+                .createNativeQuery("SELECT count(1) FROM PROFILES WHERE USER_ID=:id")
+                .setParameter("id", testId)
+                .getSingleResult();
+
+        Assert.assertEquals(chosenIndex, profileSingleResult.intValue());
     }
 }
